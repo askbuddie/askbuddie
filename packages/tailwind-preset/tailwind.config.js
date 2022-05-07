5e1693c11127021391ea1c676dd1be8f2b2d44f9
ashiishme <iamashiishy@gmail.com>
2022-05-07 19:37:28 +0200

    :sparkles: add tailwind preset & update eslint

---
+++ packages/tailwind-preset/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1536px',
+    },
+    colors: {
+      askbuddie: {
+        red: '',
+        dark: '',
+      },
+      white: '',
+      black: '',
+      gray: {
+        100: '#F2F2F2',
+        200: '#DFDADB',
+        300: '#D2CACC', // fill
+        400: '#BAAEBC', // fill
+        500: '#9B909D',
+        600: '#7A6E7C',
+        700: '#5A4662',
+        800: '#3E3246',
+        900: '#301E38',
+      },
+    },
+    fontSize: {
+      sm: ['0.875rem', { lineHeight: '1.125rem' }],
+      lg: ['1.125rem', { lineHeight: '1.5rem' }],
+      '3xl': ['2rem', { lineHeight: '2.5rem' }],
+      '4xl': ['2.625rem', { lineHeight: '3.25rem' }],
+    },
+  },
+}
