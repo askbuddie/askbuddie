5307cb0462fa053d11eed430acd183641542a81c
ashiishme <iamashiishy@gmail.com>
2022-05-07 19:36:50 +0200

    :wrench: update tailwind to use askbuddie preset

--- apps/showcase/tailwind.config.js
+++ apps/showcase/tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  presets: [require('@askbuddie/tailwind-preset')],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
