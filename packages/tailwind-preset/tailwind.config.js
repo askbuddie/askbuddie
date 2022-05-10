5afdc726802157157ea88a675648873ffc83a26f
ashiishme <iamashiishy@gmail.com>
2022-05-10 23:23:34 +0200

    :wrench: define Ask Buddie preset in Tailwind

--- packages/tailwind-preset/tailwind.config.js
+++ packages/tailwind-preset/tailwind.config.js
@@ -1,36 +1,39 @@
 module.exports = {
   theme: {
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      '2xl': '1536px',
-    },
-    colors: {
-      askbuddie: {
-        red: '',
-        dark: '',
+    extend: {
+      fontFamily: {},
+      screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1536px',
       },
-      white: '',
-      black: '',
-      gray: {
-        100: '#F2F2F2',
-        200: '#DFDADB',
-        300: '#D2CACC', // fill
-        400: '#BAAEBC', // fill
-        500: '#9B909D',
-        600: '#7A6E7C',
-        700: '#5A4662',
-        800: '#3E3246',
-        900: '#301E38',
+      colors: {
+        askbuddie: {
+          red: '#E53935',
+          black: '#2D2D2D',
+        },
+        white: '#FFFFFF',
+        black: '#000000',
+        gray: {
+          100: '#F2F2F2',
+          200: '#DFDADB',
+          300: '#D2CACC',
+          400: '#BAAEBC',
+          500: '#9B909D',
+          600: '#7A6E7C',
+          700: '#5A4662',
+          800: '#3E3246',
+          900: '#301E38',
+        },
       },
-    },
-    fontSize: {
-      sm: ['0.875rem', { lineHeight: '1.125rem' }],
-      lg: ['1.125rem', { lineHeight: '1.5rem' }],
-      '3xl': ['2rem', { lineHeight: '2.5rem' }],
-      '4xl': ['2.625rem', { lineHeight: '3.25rem' }],
+      fontSize: {
+        sm: ['0.875rem', { lineHeight: '1.125rem' }],
+        lg: ['1.125rem', { lineHeight: '1.5rem' }],
+        '3xl': ['2rem', { lineHeight: '2.5rem' }],
+        '4xl': ['2.625rem', { lineHeight: '3.25rem' }],
+      },
     },
   },
 }
