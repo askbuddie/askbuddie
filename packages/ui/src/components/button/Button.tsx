513a230b3335e50f4c331d0ff2c392deaa0831f2
ashiishme <iamashiishy@gmail.com>
2022-05-29 19:51:30 +0200

    :recycle: update button component

--- packages/ui/src/components/button/Button.tsx
+++ packages/ui/src/components/button/Button.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from 'react'
-export const Button: FC<{ text: string }> = ({ text }): JSX.Element => {
+export const Button: FC<{
+  text: string
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
+}> = ({ text, onClick }): JSX.Element => {
   return (
     <>
-      <button className="py-1 px-4 rounded-md bg-askbuddie-red text-base text-white">{text}</button>
+      <button
+        className="py-1 px-4 rounded-md bg-askbuddie-red text-base text-white"
+        onClick={onClick}
+      >
+        {text}
+      </button>
     </>
   )
 }
